6b39893dc9d06ab2158700707b5ca09a93c585d3
AidenKerr <arkerr2000@gmail.com>
2024-04-29 23:57:27 -0700

    Add minimum functionality

---
+++ src/Brainfuck.cpp
@@ -0,0 +1,65 @@
+//
+// Created by aiden on 2024-04-26.
+//
+
+#include <iostream>
+#include "Brainfuck.h"
+
+void Brainfuck::run() {
+    while (i_ptr < instructions.length()) {
+        char instruction = instructions[i_ptr];
+        process_instruction(instruction);
+    }
+    printf("done");
+}
+
+void Brainfuck::process_instruction(char instruction) {
+    switch (instruction) {
+        case '>':
+            d_ptr++;
+            break;
+        case '<':
+            d_ptr--;
+            break;
+        case '+':
+            data[d_ptr]++;
+            break;
+        case '-':
+            data[d_ptr]--;
+            break;
+        case '.':
+            printf("%c", data[d_ptr]);
+            break;
+        case ',':
+            char c;
+            std::cin >> c;
+            data[d_ptr] = c;
+            break;
+        case '[':
+            if (data[d_ptr] == 0) {
+                // jump to the matching ]
+                std::vector<int> jump_stack;
+                jump_stack.push_back(i_ptr);
+                while (!jump_stack.empty()) {
+                    i_ptr++;
+                    if (instructions[i_ptr] == '[')
+                        jump_stack.push_back(i_ptr);
+                    if (instructions[i_ptr] == ']')
+                        jump_stack.pop_back();
+                }
+            } else {
+                stack.push_back(i_ptr);
+            }
+            break;
+        case ']':
+            if (data[d_ptr] != 0) {
+                i_ptr = stack.back();
+            } else {
+                stack.pop_back();
+            }
+            break;
+        default: // other characters are treated as comments
+            break;
+    }
+    i_ptr++;
+}
---
+++ src/Brainfuck.h
@@ -0,0 +1,26 @@
+//
+// Created by aiden on 2024-04-26.
+//
+
+#ifndef BRAINFUCK_BRAINFUCK_H
+#define BRAINFUCK_BRAINFUCK_H
+
+#include <string>
+#include <vector>
+
+class Brainfuck {
+public:
+    explicit Brainfuck(std::string ins): instructions(std::move(ins)){};
+    std::string instructions;
+    std::vector<int> stack;
+    char data[3000] = {0};
+    int i_ptr = 0; // instruction pointer
+    int d_ptr = 0; // data pointer
+
+    void run();
+private:
+    void process_instruction(char instruction);
+};
+
+
+#endif //BRAINFUCK_BRAINFUCK_H
