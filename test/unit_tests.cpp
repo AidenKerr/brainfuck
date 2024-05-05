db7895dbc87345d005331a00d3915cb858e1111b
AidenKerr <arkerr2000@gmail.com>
2024-05-04 22:30:43 -0700

    Add complex loop test

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -31,3 +31,38 @@ TEST(BrainfuckTests, BasicLoop) {
     EXPECT_EQ(bf.getData(0), 0x0);
     EXPECT_EQ(bf.getData(1), 0x4);
 }
+
+TEST(BrainfuckTests, ComplexLoop) {
+    /*
+     * Testing nested loops where an inner loop doesn't run on every loop
+     * On each iteration, increment d[3]
+     * Also, alternate between incrementing d[4] and d[5]
+     * This can actually be used to check if n is even or odd, too! :)
+     *
+     * In the commented code, n=5. The actual test uses a different n
+     *
+     *  +++++>>+<<          initialize memory | 0: 5 | 1: 0 | 2: 1
+     *  [>                  outer loop (point to d[1])
+     *      [               if d(1) != 0
+     *          [-]>[-]     reset to 1: 0 | 2: 0
+     *          >>+         increment d(4)
+     *          <<<         point to d(1)
+     *      ]
+     *      >[              else (when d(1) was 0 before)
+     *          <+          set d(1) to 1
+     *          >>>>+       increment d(5)
+     *          <<<-        point to d(2) (and decrement it to escape)
+     *      ]
+     *      [-]+            reset d(2) to 1
+     *      >+<<            increment d(3)
+     *  <-]                 end outer loop (decrement counter)
+     *
+     */
+    std::string instructions = "+++++++>>+<<[>[[-]>[-]>>+<<<]>[<+>>>>+<<<-][-]+>+<<<-]";
+    Brainfuck bf(instructions);
+    bf.run();
+    EXPECT_EQ(bf.getData(0), 0x0);
+    EXPECT_EQ(bf.getData(3), 0x7);
+    EXPECT_EQ(bf.getData(4), 0x3);
+    EXPECT_EQ(bf.getData(5), 0x4);
+}
