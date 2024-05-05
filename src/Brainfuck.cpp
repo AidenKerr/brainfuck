99a967c8fca6653b5551666fec00eb2963eaba8e
AidenKerr <arkerr2000@gmail.com>
2024-05-04 22:55:10 -0700

    Add preprocessing to improve performance

--- src/Brainfuck.cpp
+++ src/Brainfuck.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <stack>
 #include "Brainfuck.h"
 
 void Brainfuck::run() {
@@ -34,25 +35,12 @@ void Brainfuck::process_instruction(char instruction) {
             break;
         case '[':
             if (data[d_ptr] == 0) {
-                // jump to the matching ]
-                std::vector<int> jump_stack;
-                jump_stack.push_back(i_ptr);
-                while (!jump_stack.empty()) {
-                    i_ptr++;
-                    if (instructions[i_ptr] == '[')
-                        jump_stack.push_back(i_ptr);
-                    if (instructions[i_ptr] == ']')
-                        jump_stack.pop_back();
-                }
-            } else {
-                stack.push_back(i_ptr);
+                i_ptr = jump_map[i_ptr];
             }
             break;
         case ']':
             if (data[d_ptr] != 0) {
-                i_ptr = stack.back();
-            } else {
-                stack.pop_back();
+                i_ptr = jump_map[i_ptr];
             }
             break;
         default: // other characters are treated as comments
@@ -63,4 +51,19 @@ void Brainfuck::process_instruction(char instruction) {
 
 char Brainfuck::getData(int index) {
     return data[index];
+}
+
+void Brainfuck::preprocess() {
+    std::stack<int> stack;
+    for (int i = 0; i < instructions.length(); i++) {
+        char c = instructions[i];
+        if (c == '[') {
+            stack.push(i);
+        } else if (c == ']') {
+            int start = stack.top();
+            stack.pop();
+            jump_map[start] = i;
+            jump_map[i] = start;
+        }
+    }
 }
--- src/Brainfuck.h
+++ src/Brainfuck.h
@@ -6,20 +6,23 @@
 #define BRAINFUCK_BRAINFUCK_H
 
 #include <string>
-#include <vector>
+#include <map>
 
 class Brainfuck {
 public:
-    explicit Brainfuck(std::string ins): instructions(std::move(ins)){};
+    explicit Brainfuck(std::string ins): instructions(std::move(ins)){
+        preprocess();
+    };
     void run();
     char getData(int index);
 private:
     std::string instructions;
-    std::vector<int> stack;
+    std::map<int, int> jump_map;
     char data[3000] = {0};
     int i_ptr = 0; // instruction pointer
     int d_ptr = 0; // data pointer
     void process_instruction(char instruction);
+    void preprocess();
 };
 
 
